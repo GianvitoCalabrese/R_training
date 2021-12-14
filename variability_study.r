83c730159ada6424f13cbcde54ff4498d09f0d76
Gianvito Calabrese <gcalabrese296@gmail.com>
2021-12-15 00:57:42 +0100

    Pushing the MASS::fitdistr()
    To consider that is different from fitdist() from other package.

--- variability_study.r
+++ variability_study.r
@@ -1,8 +1,8 @@
 # Create a sequence of numbers between -10 and 10 incrementing by 0.1.
-x <- seq(-10, 10, by = .1)
 
-y<-rnorm(100, mean = 2.5, sd = 0.5)
-
+y<-rnorm(100, mean = 2.5, sd = 0.5) -> data
+hist(data)
+MASS::fitdistr(data, "normal")
 # Choose the mean as 2.5 and standard deviation as 0.5.
 #y <- dnorm(x, mean = 2.5, sd = 0.5)
 
@@ -10,8 +10,5 @@ print(length(y))
 
 # Give the chart file a name.
 png(file = "dnorm.png")
-
-plot(x,y)
-
 
 # R < scriptName.R --no-save  
