e61bf64888921dcd2b3446557af518ae1fc0eed9
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-01-21 14:37:48 +0100

    now working with menu()...

--- R Pearson stacker generator.r
+++ R Pearson stacker generator.r
@@ -3,29 +3,37 @@ library(curl)
 library(dplyr)
 #dat = fread("https://github.com/arunsrinivasan/satrdays-workshop/raw/master/flights_2014.csv")
 dat = read.table("https://www.dropbox.com/s/w85p6egja4y3jsa/Bin%20Map%20-%20leader.txt?dl=1", header=TRUE,  sep = '\t', skipNul=TRUE)
-print(dat)
 
-param <- menu(colnames(dat), graphics=TRUE, title="Choose parameter")
+
+param_set <- function(x) {        # Create user-defined function
+ 
+  out <- menu(colnames(x), graphics=TRUE, title="Choose parameter")
+ 
+  out
+}
+
+
+param<- colnames(dat)[param_set(dat)]
+print(param)
+
 
 i=0
 x = dat[,c(param)]
 typeof(x)
 nums <- unlist(sapply(dat, is.numeric))
-vec <- names(dat[, ..nums])
+vec <- names(dat[, nums])
 
-vec
 
 for(i in vec) {
 print(i)
 }
 
-a <- air_time
-print(typeof(a))
+#a <- air_time
 
 output <- data.table(Param = factor(), Pearson = numeric())
 for(i in vec) {
 print(typeof(i))
-m <- lm(formula = get(i) ~ air_time, data=dat)
+m <- lm(formula = get(i) ~ x, data=dat)
 s <- summary(m)
 new.row <- data.frame(Param = i ,Pearson = s$r.squared )
 output <- rbind(output, new.row)
