556da63b51444b5e55b7e05225f6acee94b403ed
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-02-17 00:11:53 +0100

    minor changes...

--- variability_study.r
+++ variability_study.r
@@ -2,14 +2,14 @@
 
 runif(10, min = -1, max = 1)
 
-y<-rnorm(100, mean = 2.5, sd = 0.5) -> data
+data <-rnorm(100, mean = 2.5, sd = 0.5)
 hist(data)
-MASS::fitdistr(data, "normal")
+y <- MASS::fitdistr(data, "normal")
 # Choose the mean as 2.5 and standard deviation as 0.5.
 #y <- dnorm(x, mean = 2.5, sd = 0.5)
 
-print(length(y))
-
+print(y)
+print(y[["estimate"]][["mean"]])
 # Give the chart file a name.
 png(file = "dnorm.png")
 
