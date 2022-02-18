351b554da865deeceadf47b7db64c40e4433550d
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-02-18 08:27:01 +0100

    still ramping R libraries...

--- package.R
+++ package.R
@@ -6,3 +6,12 @@ BinMean <- function (n, every, na.rm = FALSE) {
   if (r) x <- c(x, mean.default(dist[(n - r + 1):n], na.rm = na.rm))
   x
   }
+
+
+  RW <- function(N, x0, mu, variance) {
+  z<-cumsum(rnorm(n=N, mean=0, 
+                  sd=sqrt(variance)))
+  t<-1:N
+  x<-x0+t*mu+z
+  return(x)
+  }
