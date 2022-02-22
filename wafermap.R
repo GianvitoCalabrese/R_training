3fda8bf801de589d32f870e88dbd45758ea95421
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-02-22 07:47:10 +0100

    Update wafermap.R

--- wafermap.R
+++ wafermap.R
@@ -1,10 +1,11 @@
 # load data
 madeUp=read.table("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/madeUp.csv", sep=",", header=T)
- 
+dat = read.table("https://www.dropbox.com/s/w85p6egja4y3jsa/Bin%20Map%20-%20leader.txt?dl=1", header=TRUE,  sep = '\t', skipNul=TRUE)
 # load libraries
 library(tidyverse)
- 
-# Prepare data 
+wfqty <- nrow(unique(dat$wafer))
+# Prepare data
+# X.Axis = diex, X.Axis = diey, Group can be Lot, wafers.. it's the trellis, randVals should be bin/wfsqty
 theData <- madeUp %>% 
  group_by(X.Axis, Y.Axis, Group) %>% 
  dplyr::summarize(statistic=mean(randVals, na.rm = TRUE))
