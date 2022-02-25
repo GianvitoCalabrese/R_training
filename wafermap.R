a7ee4dcf27aa1413bfa4c9c096aa9136e7fac4b7
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-02-25 22:13:00 +0100

    Update wafermap.R

--- wafermap.R
+++ wafermap.R
@@ -1,29 +1,27 @@
 # load data
-madeUp=read.table("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/madeUp.csv", sep=",", header=T)
+#madeUp=read.table("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/madeUp.csv", sep=",", header=T)
 dat = read.table("https://www.dropbox.com/s/w85p6egja4y3jsa/Bin%20Map%20-%20leader.txt?dl=1", header=TRUE,  sep = '\t', skipNul=TRUE)
 # load libraries
 library(tidyverse)
 wfqty <- length(unique(dat$wafer))
 print(wfqty)
-
-theData <- madeUp %>% 
- group_by(X.Axis, Y.Axis, Group) %>% 
- dplyr::summarize(statistic=mean(randVals/wfqty, na.rm = TRUE))
-
-theData <- madeUp %>% 
- group_by(dat$die_x, dat$die_y, dat$Bin_Name) %>% 
- dplyr::summarize(statistic=mean(randVals/wfqty, na.rm = TRUE))
-
 # Prepare data
 # X.Axis = diex, X.Axis = diey, Group can be Lot, wafers.. it's the trellis, randVals should be bin/ewfsqty
-theData <- madeUp %>% 
- group_by(X.Axis, Y.Axis, Group) %>% 
- dplyr::summarize(statistic=mean(randVals, na.rm = TRUE))
+#theData <- madeUp %>% 
+#group_by(X.Axis, Y.Axis, Group) %>% 
+#dplyr::summarize(statistic=mean(randVals/wfqty, na.rm = TRUE))
+
+theData <- dat %>% 
+    group_by(diex, diey, BinName) %>% 
+    dplyr::summarize(statistic=mean(length(BinName)/wfqty, na.rm = TRUE))
+
+
+
  
 # plot
-ggplot(theData, aes(X.Axis, Y.Axis)) +
+ggplot(theData, aes(dat$diex, dat$diey)) +
     
-    coord_cartesian(xlim = c(0,20), ylim = c(0,20)) +
+    coord_cartesian(xlim = c(min(dat$diex),max(dat$diex)), ylim = c(min(dat$diey),max(dat$diey))) +
     scale_x_continuous(breaks = seq(0,20)) +
     scale_y_continuous(breaks = seq(0,20))+
     
@@ -37,5 +35,5 @@ ggplot(theData, aes(X.Axis, Y.Axis)) +
         )+
         
     ggtitle('Wafer Map')+
-    facet_wrap(~Group)+
+    facet_wrap(~BinName)+
     scale_fill_gradientn(colors = rainbow(100))
