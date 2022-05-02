a840a9c4e896a03f8cfe10291cdf0bc03f00a125
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-05-03 00:52:17 +0200

    Update test copy 2.R

--- ForExensio/test copy 2.R
+++ ForExensio/test copy 2.R
@@ -1,12 +1,11 @@
 library(dplyr)
 library(tidyr)
-#library(party)
+library(ClusterR)
 
 dat = read.table("https://www.dropbox.com/s/w85p6egja4y3jsa/Bin%20Map%20-%20leader.txt?dl=1", header=TRUE,  sep = '\t', skipNul=TRUE)
-datfil <- dplyr::filter(dat, grepl("Id",BinName))
+resp = read.table("https://www.dropbox.com/s/hhws0dedltkgy9t/Bin%20Map%20wf%20-%20leader.txt?dl=1", header=TRUE,  sep = '\t', skipNul=TRUE)
 
-
-depr_df <- datfil %>% 
+depr_df <- dplyr::filter(dat, grepl("Id",BinName)) %>% 
   mutate(Quadrant = case_when(
     diex >= (max(dat$diex) - min(dat$diex))/2 & diey <= (max(dat$diey) - min(dat$diey))/2     ~ "PP",
     diex < (max(dat$diex) - min(dat$diex))/2 & diey <= (max(dat$diey) - min(dat$diey))/2     ~ "PN",
@@ -15,13 +14,7 @@ depr_df <- datfil %>%
     ))
 
 
-View(depr_df)
-
 grp_df <- depr_df %>% count(wafer, Quadrant, sort = TRUE)
-
-View(grp_df)
-
-
 
 
 grp_df_2 <- grp_df %>%
@@ -29,18 +22,20 @@ grp_df_2 <- grp_df %>%
               values_from = n, 
               values_fill = 0)
 
+
 View(grp_df_2)
-#This is for decision tree - maybe I would like to have categorization based on different algorithm
-## Give the chart file a name.
-#png(file = "decision_tree.png")
+View(resp)
+#df = merge(x=grp_df_2,y=resp,by="Wafer")
+
+
 #
-## Create the tree.
-#  output.tree <- ctree(
-#  nativeSpeaker ~ age + shoeSize + score, 
-#  data = input.dat)
+#X = dietary_survey_IBS[, -ncol(dietary_survey_IBS)]   # data (excluding the response variable)
 #
-## Plot the tree.
-#plot(output.tree)
+#y = dietary_survey_IBS[, ncol(dietary_survey_IBS)]    # the response variable
 #
-## Save the file.
-#dev.off()
+#dat = center_scale(X, mean_center = T, sd_scale = T)  # centering and scaling the data
+#gmm = GMM(dat, 2, dist_mode = "maha_dist", seed_mode = "random_subset", km_iter = 10,
+#          em_iter = 10, verbose = F)          
+#
+## predict centroids, covariance matrix and weights
+#pr = predict(gmm, newdata = dat)
